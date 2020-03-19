35d10a51e4c75f3c055a3f882dab488651206b12
FluffyBaloo <michael.a.rodriguez@sjsu.edu>
2020-03-19 00:13:53 -0700

    Update sketch.js
    commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 function setup() {
   createCanvas(400, 400);
+  background(220);
 }
 
 function draw() {
-  background(220);
+
+if (mouseIsPressed){
+  stroke(map(mouseX, 0, 600, RGB, 0, 255, true))
+  quad(mouseX, mouseY, pmouseX, pmouseY);
+  triangle(width - mouseX, height = mouseY, width - pmouseX, height = pmouseY);
+
+}
+
 }
