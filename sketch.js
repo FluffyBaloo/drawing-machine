627876e38b0b5d093d1550b2e76a1d6220251d23
FluffyBaloo <michael.a.rodriguez@sjsu.edu>
2020-03-19 20:33:56 -0700

    commit

--- sketch.js
+++ sketch.js
@@ -1,15 +1,30 @@
 function setup() {
   createCanvas(400, 400);
   background(220);
+
+  strokeWeight(15);
 }
 
 function draw() {
 
 if (mouseIsPressed){
   stroke(map(mouseX, 0, 600, RGB, 0, 255, true))
-  quad(mouseX, mouseY, pmouseX, pmouseY);
-  triangle(width - mouseX, height = mouseY, width - pmouseX, height = pmouseY);
+  line(mouseX, mouseY, pmouseX, pmouseY);
+  line(width - mouseX, height = mouseY, width - pmouseX, height = pmouseY);
+  square(mouseX, mouseY, pmouseX, pmouseY);
+  square(width - mouseX, height = mouseY, width - pmouseX, height = pmouseY);
 
 }
+
+}
+
+function keyTyped(){
+
+if (key === 's'){
+  // saves image
+  saveCanvas('fileName', 'png');
+}
+
+return false;
 
 }
